dee3dc8403a6d93c1d1bac40b4dd55b73dd84627
Денис Сахаров <sakhirsp@gmail.com>
2022-03-24 23:11:37 +0300

    HERO AND ENEMY ONLINE

--- GGG.cpp
+++ GGG.cpp
@@ -1,4 +1,6 @@
-﻿#include <iostream>
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
@@ -8,15 +10,22 @@
 #include <string.h>
 #include <vector>
 #include "Cell.hpp"
-
+//#pragma comment(lib, "ws2_32.lib")
+//#include <winsock2.h>
+//#include <iostream>
+//
+//#pragma warning(disable: 4996)
 
 using namespace std;
 //структура хранения нажатия клавиш управления
 enum eDir
 {
-	STOP = 0, LEFT, RIGHT, UP, DOWN, LEFTS, RIGHTS, UPS, DOWNS, LEFTT, RIGHTT, UPT, DOWNT, LEFTST, RIGHTST, UPST, DOWNST
+	STOP = 0, LEFT, RIGHT, UP, DOWN, LEFTS, RIGHTS, UPS, DOWNS
 };
 eDir dir;
+
+SOCKET Connection;
+
 //класс карты
 class Map
 {
@@ -24,7 +33,7 @@ public:
 	Map(int height = 20, int width = 40);
 	~Map();
 	void addHero(int y, int x);
-	void addHeroTwo(int y, int x);
+	/*void addHeroTwo(int y, int x);*/
 	void addEnemy(int y, int x);
 
 	void Draw() {
@@ -41,7 +50,7 @@ public:
 					}
 					else (((Shoot*)cells[y][x])->lifetime)--;
 				}
-				if (strcmp(cells[y][x]->getType(), "ShootTwo") == 0) {
+				/*if (strcmp(cells[y][x]->getType(), "ShootTwo") == 0) {
 					if (((ShootTwo*)cells[y][x])->lifetimeT == 0)
 					{
 						delete cells[y][x];
@@ -49,7 +58,7 @@ public:
 
 					}
 					else (((ShootTwo*)cells[y][x])->lifetimeT)--;
-				}
+				}*/
 				cout << cells[y][x]->Image();
 			}
 			cout << endl;
@@ -59,14 +68,14 @@ public:
 		if (x2 < 0 || y2 < 0 || x2 >= width || y2 >= height) return;
 		if (strcmp(cells[y2][x2]->getType(), "Enemy") == 0) return;
 		if (strcmp(cells[y2][x2]->getType(), "Hero") == 0) return;
-		if (strcmp(cells[y2][x2]->getType(), "HeroTwo") == 0) return;
+		/*if (strcmp(cells[y2][x2]->getType(), "HeroTwo") == 0) return;*/
 		delete cells[y2][x2];
 		cells[y2][x2] = cells[y1][x1];
 		cells[y1][x1] = new Cell(y1, x1, this);
 		cells[y2][x2]->x = x2;
 		cells[y2][x2]->y = y2;
 	}
-	void MoveT(int x1, int y1, int x2, int y2) {
+	/*void MoveT(int x1, int y1, int x2, int y2) {
 		if (x2 < 0 || y2 < 0 || x2 >= width || y2 >= height) return;
 		if (strcmp(cells[y2][x2]->getType(), "Enemy") == 0) return;
 		if (strcmp(cells[y2][x2]->getType(), "Hero") == 0) return;
@@ -76,25 +85,28 @@ public:
 		cells[y1][x1] = new Cell(y1, x1, this);
 		cells[y2][x2]->x = x2;
 		cells[y2][x2]->y = y2;
-	}
+	}*/
 	void Run();
 	void makeShoot(int y, int x) {
 		delete cells[y][x];
 		cells[y][x] = new Shoot(y, x, this);
 
 	}
-	void makeShootT(int y, int x) {
+	/*void makeShootT(int y, int x) {
 		delete cells[y][x];
 		cells[y][x] = new ShootTwo(y, x, this);
 
-	}
+	}*/
 private:
 	Cell*** cells;
 	int height;
 	int width;
 	Hero* hero = NULL;
-	HeroTwo* heroT = NULL;
+	Enemy* enemy = NULL;
+	//HeroTwo* heroT = NULL;
+	eDir dirEnemyAction= STOP;
 	friend class Enemy;
+	friend void ClientHandler();
 };
 //начальный класс базовой ячейки
 
@@ -108,9 +120,9 @@ Cell::~Cell()
 Hero::~Hero()
 {
 }
-HeroTwo::~HeroTwo()
-{
-}
+//HeroTwo::~HeroTwo()
+//{
+//}
 //класс Врага, наследуемый от класса Ячейки
 
 
@@ -123,9 +135,9 @@ Enemy::~Enemy()
 Shoot::~Shoot()
 {
 }
-ShootTwo::~ShootTwo()
-{
-}
+//ShootTwo::~ShootTwo()
+//{
+//}
 
 
 Map::Map(int height, int width)
@@ -151,14 +163,15 @@ void Map::addHero(int y, int x) {
 	cells[y][x] = new Hero(y,x,this);
 	hero =(Hero*) cells[y][x];
 }
-void Map::addHeroTwo(int y, int x) {
-	delete cells[y][x];
-	cells[y][x] = new HeroTwo(y, x, this);
-	heroT = (HeroTwo*)cells[y][x];
-}
+//void Map::addHeroTwo(int y, int x) {
+//	delete cells[y][x];
+//	cells[y][x] = new HeroTwo(y, x, this);
+//	heroT = (HeroTwo*)cells[y][x];
+//}
 void Map::addEnemy(int y, int x) {
 	delete cells[y][x];
 	cells[y][x] = new Enemy(y, x, this);
+	enemy = (Enemy*)cells[y][x];
 }
 
 void Map::Run() {
@@ -167,7 +180,7 @@ void Map::Run() {
 	{
 		//стирание экрана
 		system("cls");
-		vector <Enemy*> enemies;
+		/*vector <Enemy*> enemies;
 		for (int x = 0; x < width; x++)
 		{
 			for (int y = 0; y < height; y++)
@@ -181,10 +194,12 @@ void Map::Run() {
 		for (Enemy* enemy : enemies)
 		{
 			enemy->Move();
-		}
+		}*/
 		//ввод данных
 		if (_kbhit()) {
-			switch (_getch())
+			char ch = _getch();
+			send(Connection, &ch, 1, NULL);
+			switch (ch)
 			{
 				//хотьба первым игроком
 			case 'w':
@@ -200,7 +215,7 @@ void Map::Run() {
 				dir = RIGHT;
 				break;
 				//хотьба правым игроком
-			case 'i':
+			/*case 'i':
 				dir = UPT;
 				break;
 			case 'k':
@@ -211,7 +226,7 @@ void Map::Run() {
 				break;
 			case 'l':
 				dir = RIGHTT;
-				break;
+				break;*/
 				//стрельба первого игрока
 			case 'D':
 				dir = RIGHTS;
@@ -226,7 +241,7 @@ void Map::Run() {
 				dir = LEFTS;
 				break;
 
-			case 'L':
+			/*case 'L':
 				dir = RIGHTST;
 				break;
 			case 'I':
@@ -237,7 +252,7 @@ void Map::Run() {
 				break;
 			case 'J':
 				dir = LEFTST;
-				break;
+				break;*/
 
 			}
 		}
@@ -256,7 +271,7 @@ void Map::Run() {
 		case DOWN:
 			Move(hero->x, hero->y, hero->x, hero->y + 1);
 			break;
-		case LEFTT:
+		/*case LEFTT:
 			MoveT(heroT->x, heroT->y, heroT->x - 1, heroT->y);
 			break;
 		case RIGHTT:
@@ -267,7 +282,7 @@ void Map::Run() {
 			break;
 		case DOWNT:
 			MoveT(heroT->x, heroT->y, heroT->x, heroT->y + 1);
-			break;
+			break;*/
 		case LEFTS:
 			for (int x = hero->x - 1; x >= 0; x--)
 			{
@@ -308,7 +323,7 @@ void Map::Run() {
 				makeShoot(y, hero->x);
 			}
 			break;
-		case LEFTST:
+		/*case LEFTST:
 			for (int x = heroT->x - 1; x >= 0; x--)
 			{
 				if (strcmp(cells[heroT->y][x]->getType(), "Cell") != 0) {
@@ -347,14 +362,78 @@ void Map::Run() {
 				}
 				makeShootT(y, heroT->x);
 			}
-			break;
+			break;*/
 		}
-		dir = STOP;
+		dir = STOP;  
+
+
+
+
+
+		switch (dirEnemyAction)
+		{
+		case LEFT:
+			Move(enemy->x, enemy->y, enemy->x - 1, enemy->y);
+			break;
+		case RIGHT:
+			Move(enemy->x, enemy->y, enemy->x + 1, enemy->y);
+			break;
+		case UP:
+			Move(enemy->x, enemy->y, enemy->x, enemy->y - 1);
+			break;
+		case DOWN:
+			Move(enemy->x, enemy->y, enemy->x, enemy->y + 1);
+			break;
+		
+		case LEFTS:
+			for (int x = enemy->x - 1; x >= 0; x--)
+			{
+				if (strcmp(cells[enemy->y][x]->getType(), "Cell") != 0) {
+					makeShoot(enemy->y, x);
+					break;
+				}
+				makeShoot(enemy->y, x);
+			}
+			break;
+		case RIGHTS:
+			for (int x = enemy->x + 1; x < width; x++)
+			{
+				if (strcmp(cells[enemy->y][x]->getType(), "Cell") != 0) {
+					makeShoot(enemy->y, x);
+					break;
+				}
+				makeShoot(enemy->y, x);
+			}
+			break;
+		case UPS:
+			for (int y = enemy->y - 1; y >= 0; y--)
+			{
+				if (strcmp(cells[y][enemy->x]->getType(), "Cell") != 0) {
+					makeShoot(y, enemy->x);
+					break;
+				}
+				makeShoot(y, enemy->x);
+			}
+			break;
+		case DOWNS:
+			for (int y = enemy->y + 1; y < height; y++)
+			{
+				if (strcmp(cells[y][enemy->x]->getType(), "Cell") != 0) {
+					makeShoot(y, enemy->x);
+					break;
+				}
+				makeShoot(y, enemy->x);
+			}
+			break;
+			
+		}
+		dirEnemyAction = STOP;
 		Draw();
+
 	}
 }
 	
-	void Enemy::Move() {
+	/*void Enemy::Move() {
 		int sr = rand() % 20;
 		if (sr < 4) direct = sr;
 		switch (direct)
@@ -372,15 +451,89 @@ void Map::Run() {
 			map->Move(x, y, x, y + 1);
 			break;
 		}
+	}*/
+
+
+#pragma warning(disable: 4996)
+
+	Map game(20, 40);
+
+	void ClientHandler() {
+		char msg[256];
+		while (true) {
+			recv(Connection, msg, sizeof(msg), NULL);
+			switch (msg[0])
+			{
+				//хотьба врага
+			case 'w':
+				game.dirEnemyAction = UP;
+				break;
+			case 's':
+				game.dirEnemyAction = DOWN;
+				break;
+			case 'a':
+				game.dirEnemyAction = LEFT;
+				break;
+			case 'd':
+				game.dirEnemyAction = RIGHT;
+				break;
+				//стрельба врага
+			case 'D':
+				game.dirEnemyAction = RIGHTS;
+				break;
+			case 'W':
+				game.dirEnemyAction = UPS;
+				break;
+			case 'S':
+				game.dirEnemyAction = DOWNS;
+				break;
+			case 'A':
+				game.dirEnemyAction = LEFTS;
+				break;
+			}
+		}
 	}
+
 	int main() {
-		srand(unsigned int(time(NULL)));
-		Map game(20, 40);
-		game.addHero(10, 20);
-		game.addHeroTwo(2, 20);
-		game.addEnemy(15, 30);
-		game.addEnemy(5, 17);
-		game.addEnemy(15, 17);
+		//WSAStartup
+		WSAData wsaData;
+		WORD DLLVersion = MAKEWORD(2, 1);
+		if (WSAStartup(DLLVersion, &wsaData) != 0) {
+			std::cout << "Error" << std::endl;
+			exit(1);
+		}
+
+		SOCKADDR_IN addr;
+		int sizeofaddr = sizeof(addr);
+		addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+		addr.sin_port = htons(1111);
+		addr.sin_family = AF_INET;
+
+		Connection = socket(AF_INET, SOCK_STREAM, NULL);
+		if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+			std::cout << "Error: failed connect to server.\n";
+			return 1;
+		}
+		std::cout << "Connected!\n";
+
+		
+
+		char msg1[256];
+	
+		recv(Connection, msg1, sizeof(msg1), NULL);
+		int x1 = atoi(msg1);
+		recv(Connection, msg1, sizeof(msg1), NULL);
+		int y1 = atoi(msg1);
+		recv(Connection, msg1, sizeof(msg1), NULL);
+		int x2 = atoi(msg1);
+		recv(Connection, msg1, sizeof(msg1), NULL);
+		int y2 = atoi(msg1);
+		Sleep(100);
+		game.addHero(y1, x1);
+		game.addEnemy(y2, x2);
+
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, (LPVOID)(0), NULL, NULL);
+
 		game.Run();
 	}
 	
